07c7b95ab2ac71da4e776518f158c90df9543b19
notlbock <uyenla1224@gmail.com>
2025-12-18 20:38:26 -0800

    Create vectors.hpp

---
+++ vectors.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+class vectors {
+public:
+  vector<int> addingNums;
+  vector<int> subtractingNums;
+  vector<int> multiplyingNums;
+  vector<int> divdingNums;
+  vector<int> moduloNums;
+};
